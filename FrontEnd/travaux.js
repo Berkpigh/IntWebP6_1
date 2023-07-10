8ab251cb7092690203eddbaf47274da53cf957a5
Pierre Berkovits <pierre.berkovits@netplus>
2023-07-10 22:00:50 +0200

    createFigures

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -1,47 +1,21 @@
+const por = document.getElementById("portfolio");
+let gal = document.querySelector(".gallery");
 let wors = "";
 let cats = "";
 let docs = "";
-let res = "";
-/* async function fetchData(purl) {
-        let response = await fetch(purl);
-        if (!(response.ok)) {
-            console.log("La réponse " + response.ok);
-            throw new Error(`Fetch ${purl}`);
-        } else {
-            let res = await response.json();
-            console.log(res);
-            return res;
-        };
-}
- */
-async function fetchData(purl) {
-    let response = await fetch(purl);
-    if (!(response.ok)) {res = "false";}
-    else {res = await response.json();}
-    console.log(res);
-    if (res === "false") {
-        throw new Error("Erreur dans fetchData");
-    }
-    return res;
-}
-function fetchAll() {
-    try {
-        console.log("Début fetchAll");
-        let urls = `http://localhost:5678/api/works`
-        wors = fetchData(urls);
+let wmax = 0;
+let cmax = 0;
 
-        urls = `http://localhost:5678/api/categories`
-        cats = fetchData(urls);
+let urls = `http://localhost:5678/api/works`
+let response = await fetch(urls);
+wors = await response.json();
+wmax = wors.length -1;
 
-        //urls = `http://localhost:5678/api/docs`
-        //docs = fetchData(urls);
+urls = `http://localhost:5678/api/categories`
+response = await fetch(urls);
+cats = await response.json();
+cmax = cats.length -1;
 
-        return true;
-    } catch (error) {
-        console.log(error.message);
-        return false;
-    }
-}
 function removeFigures() {
     try {
         console.log("Début removeFigures");
@@ -55,18 +29,32 @@ function removeFigures() {
 function createFigures()  {
     try {
         console.log("Début createFigures");
+        gal = document.createElement("div");
+        gal.classList.add("gallery");
+        for (let w = 0; w <= wmax; w++) {
+            let fig = document.createElement("figure");
+            let ima = document.createElement("img");
+            let fic = document.createElement("figcaption");
+            ima.src = wors[w].imageUrl;
+            ima.alt = wors[w].title;
+            fic.innerHTML  = wors[w].title;
+            fig.appendChild(ima);
+            fig.appendChild(fic);
+            gal.appendChild(fig);
+            //console.log(gal);
+        }
+        por.appendChild(gal);
+        console.log(por);
         return true;
     } catch (error) {
         console.log("Erreur createFigures " + error.message);
     }
 }
 function main() {
-    let b = fetchAll();
-    if (b === true) {console.log("fetchAll Ok");
-            b = removeFigures()};
+    let b = removeFigures();
     if (b === true) {console.log("removeFigures Ok");
-            b = createFigures()};
+                     b = createFigures()};
     if (b === true) {console.log("createFigures Ok")};
 }
-const wmax = wors.length - 1;
+
 main();
